dcea3815378d9d67d537c4f4ce1166c8a008a538
Mateus Pereira <mateus.santos.p.74.v@gmail.com>
2024-08-04 15:35:27 -0300

    colocando aws na api

--- alarms/api/viewset.py
+++ alarms/api/viewset.py
@@ -5,7 +5,7 @@ from .functions import *
 @api_view(['GET', 'POST'])
 def ec2(request):
     if request.method == 'GET':
-        # create alarme
         return Response(list_automation(name='NewRunbook'))
     if request.method == 'POST':
-        return Response(execution_automation(name='NewRunbook'))
+         # create alarme
+        return Response(execution_automation(name=request.data['Name']))
